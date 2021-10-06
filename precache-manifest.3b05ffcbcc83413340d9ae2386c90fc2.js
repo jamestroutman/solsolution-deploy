1c0e3ee32ccf98d6144cc3e6cd7cb0ec8dd43614
qrails-jtroutman <jtroutman@qrails.com>
2021-10-06 11:47:31 -0500

    Updates

---
+++ precache-manifest.3b05ffcbcc83413340d9ae2386c90fc2.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266c059fe0fbbbe83f66b5356770e2be",
+    "url": "/solsolution-deploy/index.html"
+  },
+  {
+    "revision": "cbd99816bb6e0a54ca28",
+    "url": "/solsolution-deploy/static/css/main.bdac6c75.chunk.css"
+  },
+  {
+    "revision": "6cc3e8319bc2781d3ed9",
+    "url": "/solsolution-deploy/static/js/2.3f4f619a.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/solsolution-deploy/static/js/2.3f4f619a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd99816bb6e0a54ca28",
+    "url": "/solsolution-deploy/static/js/main.48a40768.chunk.js"
+  },
+  {
+    "revision": "d03fd9daab4347fe99c3",
+    "url": "/solsolution-deploy/static/js/runtime-main.a7c5ad4e.js"
+  },
+  {
+    "revision": "7dea5d2671052088eb319846645b10dc",
+    "url": "/solsolution-deploy/static/media/000_White_Gloss.7dea5d26.jpeg"
+  },
+  {
+    "revision": "a936a4b07226f93ed0c2b989050f3b19",
+    "url": "/solsolution-deploy/static/media/001_Black_Stone.a936a4b0.jpeg"
+  },
+  {
+    "revision": "166489c334f1902d22ab5cdd7e55cb8e",
+    "url": "/solsolution-deploy/static/media/alpha_white_few.166489c3.jpeg"
+  },
+  {
+    "revision": "98f73239f1678251f6e29acf1b2eca37",
+    "url": "/solsolution-deploy/static/media/alpha_white_many.98f73239.jpeg"
+  },
+  {
+    "revision": "806ac001b4183c15f08380b4f1b5b929",
+    "url": "/solsolution-deploy/static/media/color_grid.806ac001.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/solsolution-deploy/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/solsolution-deploy/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/solsolution-deploy/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/solsolution-deploy/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/solsolution-deploy/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/solsolution-deploy/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/solsolution-deploy/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/solsolution-deploy/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/solsolution-deploy/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/solsolution-deploy/static/media/logo.2810a88b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61eb4f88ec5c28e3f5fbdef8e0888821.js"
+  "/solsolution-deploy/precache-manifest.3b05ffcbcc83413340d9ae2386c90fc2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/solsolution-deploy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
